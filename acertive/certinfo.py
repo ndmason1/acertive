9c2cd21f5061a2bdfe457e6a9c5dac59cde66214
Nigel Mason <ndmason@live.ca>
2015-01-19 16:57:06 -0500

    switch to pyOpenSSL to parse certs. pem, der and PKCS12 formats can be parsed.

--- acertive/certinfo.py
+++ acertive/certinfo.py
@@ -1,7 +1,36 @@
 
-from pyasn1_modules import pem, rfc2459
-from pyasn1.codec.der import decoder
+from OpenSSL import crypto
+from sys import argv
+import getpass
 
-substrate = pem.readPemFromFile(open('../dummy.crt'))
-cert = decoder.decode(substrate, asn1Spec=rfc2459.Certificate())[0]
-print(cert.prettyPrint())
+def loadPem(fname):
+	pem = open(fname,'rb').read()
+	cert = crypto.load_certificate(crypto.FILETYPE_PEM, pem)
+	print "opened PEM cert, notAfter is: ", cert.get_notAfter()
+
+def loadDer(fname):
+	der = open(fname,'rb').read()
+	cert = crypto.load_certificate(crypto.FILETYPE_ASN1, der)
+	print "opened DER cert, notAfter is: ", cert.get_notAfter()
+
+def loadPfx(fname):	
+	pfx = open(fname,'rb').read()
+	pw = getpass.getpass("Export password: ")	
+	try:
+		cert = crypto.load_pkcs12(pfx,pw).get_certificate()
+		print "opened PKCS12 cert, notAfter is: ", cert.get_notAfter()
+	except crypto.Error:
+		print "bad password! aborting" # TODO allow multiple tries
+	
+
+if __name__=='__main__':
+	if (len(argv) > 1):
+		print "processing cert..."
+		if (argv[1][-3:] in 'p12', 'pfx'):
+			loadPfx(argv[1])
+		elif (argv[1][-3:] in 'pem', 'crt'):
+			loadPem(argv[1])
+		elif (argv[1][-3:] in 'der', 'cer'):
+			loadDer(argv[1])
+	else:
+		print "Please specify a file!"
