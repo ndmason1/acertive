d970ab1206fc3bce3d91357cd56db4bd0642b458
Nigel Mason <ndmason@live.ca>
2015-01-31 19:14:20 -0500

    open files using 'with' stmt

--- acertive/cert.py
+++ acertive/cert.py
@@ -14,26 +14,26 @@ def loadCert(fname):
 	return cert
 
 def loadDer(fname):
-	der = open(fname,'rb').read()
-	cert = crypto.load_certificate(crypto.FILETYPE_ASN1, der)
-	print "opened DER cert, notAfter is: ", cert.get_notAfter()
-	return cert
+	with open(fname,'rb') as der:
+		cert = crypto.load_certificate(crypto.FILETYPE_ASN1, der.read())
+		print "opened DER cert, notAfter is: ", cert.get_notAfter()
+		return cert
 
 def loadPem(fname):
-	pem = open(fname,'rb').read()
-	cert = crypto.load_certificate(crypto.FILETYPE_PEM, pem)
-	print "opened PEM cert, notAfter is: ", dateparser.parseUTCDate(cert.get_notAfter())
-	return cert
-
-def loadP12(fname):
-	p12 = open(fname,'rb').read()
-	pw = getpass.getpass("Export password: ")	
-	try:
-		cert = crypto.load_pkcs12(p12,pw).get_certificate()
-		print "opened PKCS12 cert, notAfter is: ", dateparser.parseUTCDate(cert.get_notAfter())
+	with open(fname,'rb') as pem:	
+		cert = crypto.load_certificate(crypto.FILETYPE_PEM, pem.read())
+		print "opened PEM cert, notAfter is: ", dateparser.parseUTCDate(cert.get_notAfter())
 		return cert
-	except crypto.Error:
-		print "bad password! aborting" # TODO allow multiple tries
+
+def loadP12(fname):	
+	with open(fname,'rb') as p12:
+		pw = getpass.getpass("Export password: ")	
+		try:
+			cert = crypto.load_pkcs12(p12.read(),pw).get_certificate()
+			print "opened PKCS12 cert, notAfter is: ", dateparser.parseUTCDate(cert.get_notAfter())
+			return cert
+		except crypto.Error:
+			print "bad password! aborting" # TODO allow multiple tries
 	
 
 if __name__=='__main__':
