d7861703fb5a78a16d5150357545db8018fe7607
Nigel Mason <ndmason@live.ca>
2015-03-30 15:31:15 -0400

    setup tasks: create certs file, set initial config

--- setup.py
+++ setup.py
@@ -1,21 +1,40 @@
 import os
 from setuptools import setup
 from setuptools.command.install import install
+from ConfigParser import ConfigParser
 
 class InstallTasks(install):
     """ set up files """
-    def run(self):
-        # auto search for certs
-
-        # create user,group for daemon
-
-        # create certs.json
-        dataDir = '/etc/acertive/'
-        if not os.path.exists(dataDir):
-            os.makedirs(dataDir)
-            open(dataDir+'certs.json', 'a').close()
+    def run(self):        
         
-        # add to init.d
+        uid = int(os.getenv('SUDO_UID', '0'))
+        gid = int(os.getenv('SUDO_GID', '0'))
+
+        # create config file
+        conf_file = 'config.cfg'
+        data_dir = '/etc/acertive/'
+        certs_file = 'acertive_tracked.json'
+        # if not os.path.isfile(conf_file):
+        conf = ConfigParser()
+        conf.add_section('MAIN')
+        conf.set('MAIN','notifyMethod','log')
+        conf.set('MAIN','storedCertsFile', os.path.join(data_dir,certs_file))
+        conf.set('MAIN','weeklyThreshold','60')
+        conf.set('MAIN','dailyThreshold','14')
+
+        conf.add_section('MAIL')
+        conf.set('MAIL','SMTPServerName','localhost')
+        conf.set('MAIL','notifyAddrs','')
+        conf.set('MAIL','useTLS', 0)
+
+        with open(conf_file, 'wb') as cfile:
+            conf.write(cfile)
+                
+        # create tracked certs file        
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)            
+        open(os.path.join(data_dir,certs_file), 'a').close()
+        os.chown(os.path.join(data_dir,certs_file), uid, gid)
 
         install.run(self)
 
@@ -45,5 +64,8 @@ setup(
     },
     cmdclass={
         'install': InstallTasks
-    }
+    },
+    # data_files=[
+    #     ('/etc/init.d', ['init-script'])
+    # ]
 )
